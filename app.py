cc0a10ff236cf1edc2b94a19872a482d9435f0f0
Romain DUMOULIN <rdumoulin@lis47.fr>
2025-10-16 11:27:56 +0200

    Fix: mise à jour Flask + corrections vulnérabilités

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask, request, render_template
-import os
+import os, subprocess
 import sqlite3
+from markupsafe import escape
 
 app = Flask(__name__)
 
-# Vulnérabilité 1 : Injection SQL
+# Vulnérabilité 1 (fixé): Injection SQL
 def get_user_from_db(username):
     conn = sqlite3.connect('users.db')
     c = conn.cursor()
-    query = f"SELECT * FROM users WHERE username = '{username}'"  # ❌ injection volontaire
-    c.execute(query)
+    query = "SELECT * FROM users WHERE username = ?"
+    c.execute(query, (username,))
     return c.fetchall()
 
 @app.route("/")
@@ -18,15 +19,17 @@ def index():
 
 @app.route("/search")
 def search():
-    username = request.args.get("user")
+    username = request.args.get("user", "")
     results = get_user_from_db(username)
-    return f"Résultats pour {username}: {results}"
+    return f"Résultats pour {escape(username)}: {results}"
 
-# Vulnérabilité 2 : Commande système
+
+# Vulnérabilité 2 (fixé) : Commande système
 @app.route("/ping")
 def ping():
     host = request.args.get("host")
-    return os.popen(f"ping -c 1 {host}").read()  # ❌ injection de commande
+    result = subprocess.run(["ping", "-c", "1", host], capture_output=True, text=True)
+    return result.stdout
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run
